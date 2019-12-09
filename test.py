e26f142f1399b033a72889e423d65d2d41bcca3e
root <root@shagen2.ru>
2019-12-09 16:00:05 +0000

    test

---
+++ test.py
@@ -0,0 +1,11 @@
+#!/bin/python
+ip = raw_input("vvedi diapzon ip:")
+a = ip.split('-')
+div = [a[0]]
+l_a = a[1].split('.')
+f_a = a[0].split('.')
+b = int(l_a[3]) - int(f_a[3])
+for ips in range(b):
+  f_a[3] = str(int(f_a[3]) + 1)
+  div.append(".".join(f_a))
+print (div)	
