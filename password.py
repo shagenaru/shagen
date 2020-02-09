f78357a501bd3955aa153449a73df71f852d2937
root <root@shagen2.ru>
2020-02-09 12:04:05 +0000

    Revert "bagfix"
    This reverts commit 04129816257b0830823e76e0cfc31fe32a0a2953.

--- password.py
+++
@@ -1,9 +0,0 @@
-#!/bin/python
-import re
-
-pas = raw_input("vvedi password:")
-a = re.match(r'^(?=.*[0-9].*)(?=.*[a-z].*)(?=.*[A-Z].*)[0-9a-zA-Z]{8,}$',pas)
-print(a)
-if a!=None:
-	print ("Ok")
-else: print ("No ok!")
